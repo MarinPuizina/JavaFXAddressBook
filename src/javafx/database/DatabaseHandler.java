4a9705147d8d2c36b8192168a4342b96e87af5fb
MarinPuizina <marinpuizina@hotmail.com>
2020-06-06 21:46:43 +0200

    Add createCreate connection method

--- src/javafx/database/DatabaseHandler.java
+++ src/javafx/database/DatabaseHandler.java
@@ -1,9 +1,37 @@
 package javafx.database;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author marin
  */
 public class DatabaseHandler {
+    
+    private static DatabaseHandler handler;
+    
+    private static String dbURL = "jdbc:derby:database;create=true";
+    private static Connection conn = null;
+    private static Statement stmt = null;
+    final String dbDriver = "org.apache.derby.jdbc.EmbeddedDriver";
+    
+    /**
+     * Method for creating database connection
+     */
+    void createConnection() {
+        
+        try {
+            Class.forName(dbDriver).newInstance();
+            conn = DriverManager.getConnection(dbURL);
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Cant load database", "Database Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        
+    }
     
 }
