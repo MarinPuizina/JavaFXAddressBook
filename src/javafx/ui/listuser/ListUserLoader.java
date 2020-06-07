6a1d5da6d21793a1767bd81c3cf957d4de6f1802
MarinPuizina <marinpuizina@hotmail.com>
2020-06-07 20:14:43 +0200

    Add ListUserLoader class to make it easier to check the table view data insert functionality

---
+++ src/javafx/ui/listuser/ListUserLoader.java
@@ -0,0 +1,33 @@
+package javafx.ui.listuser;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author marin
+ */
+public class ListUserLoader extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("ListUser.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }
+    
+    public static void main(String[] args) {
+        launch(args);        
+    }
+
+}
