63d5ea84f7f4b6ff4b7273695132309c73a6cf16
MarinPuizina <marinpuizina@hotmail.com>
2020-06-08 19:41:36 +0200

    Add loadWindow method in MainController.java

--- src/javafx/ui/main/MainController.java
+++ src/javafx/ui/main/MainController.java
@@ -1,10 +1,18 @@
 package javafx.ui.main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -27,6 +35,22 @@ public class MainController implements Initializable {
 
     @FXML
     private void loadAddUser(ActionEvent event) {
+    }
+    
+    void loadWindow(String location, String title) {
+        
+        try {
+            
+            Parent parent = FXMLLoader.load(getClass().getResource(location));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle(title);
+            stage.setScene(new Scene(parent));
+            stage.show();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
