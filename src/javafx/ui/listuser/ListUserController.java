948ec5bcacd6fb76e19b7dd7039b6c50772f368c
MarinPuizina <marinpuizina@hotmail.com>
2020-06-09 23:07:27 +0200

    Add functionality for deleteing the user

--- src/javafx/ui/listuser/ListUserController.java
+++ src/javafx/ui/listuser/ListUserController.java
@@ -3,6 +3,7 @@ package javafx.ui.listuser;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,6 +14,8 @@ import javafx.database.DatabaseHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,7 +30,7 @@ import javafx.ui.adduser.AddUserController;
 public class ListUserController implements Initializable {
 
     ObservableList<User> list = FXCollections.observableArrayList();
-    
+
     @FXML
     private AnchorPane rootPane;
     @FXML
@@ -44,67 +47,110 @@ public class ListUserController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         initColumns();
-        loadData();  
-        
-    }    
- 
+        loadData();
+
+    }
+
     private void initColumns() {
-        
+
         firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
         lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
         emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
-        
+
     }
 
     private void loadData() {
-        
+
         DatabaseHandler databaseHandler = DatabaseHandler.getInstance();
-        
+
         String qu = "SELECT * FROM PERSON";
         ResultSet resultSet = databaseHandler.execQuery(qu);
-        
-        
+
         try {
-                
-            while(resultSet.next()) {
-                
-                    String firstName = resultSet.getString("firstName");
-                    String lastName = resultSet.getString("lastName");
-                    String email = resultSet.getString("email");
-                    
-                    list.add(new User(firstName, lastName, email));
-                    
+
+            while (resultSet.next()) {
+
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                String email = resultSet.getString("email");
+
+                list.add(new User(firstName, lastName, email));
+
             }
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(AddUserController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         tableView.getItems().setAll(list);
-        
+
     }
-    
+
     @FXML
     private void handleUserDeleteOption(ActionEvent event) {
-        
-        
-        
+
+        User selectedForDeletion = tableView.getSelectionModel().getSelectedItem();
+
+        if (selectedForDeletion == null) {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("User was not selected, please select an user.");
+            alert.showAndWait();
+
+            return;
+        }
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Deleting User");
+        alert.setContentText("Are you sure you want to delete the user: " + selectedForDeletion.getEmail());
+        Optional<ButtonType> answer = alert.showAndWait();
+
+        if (answer.get() == ButtonType.OK) {
+
+            Boolean result = DatabaseHandler.getInstance().deleteUser(selectedForDeletion.getEmail());
+
+            if (result) {
+
+                Alert cancelAlert = new Alert(Alert.AlertType.INFORMATION);
+                cancelAlert.setHeaderText(null);
+                cancelAlert.setContentText("User has been deleted");
+                cancelAlert.showAndWait();
+
+            } else {
+
+                Alert cancelAlert = new Alert(Alert.AlertType.INFORMATION);
+                cancelAlert.setHeaderText(null);
+                cancelAlert.setContentText("Delete process has failed");
+                cancelAlert.showAndWait();
+                
+            }
+
+        } else {
+
+            Alert cancelAlert = new Alert(Alert.AlertType.INFORMATION);
+            cancelAlert.setHeaderText(null);
+            cancelAlert.setContentText("Delete process canceled");
+            cancelAlert.showAndWait();
+
+        }
+
     }
-    
+
     public static class User {
-        
+
         private final SimpleStringProperty firstName;
         private final SimpleStringProperty lastName;
         private final SimpleStringProperty email;
-        
+
         User(String firstName, String lastName, String email) {
-            
+
             this.firstName = new SimpleStringProperty(firstName);
             this.lastName = new SimpleStringProperty(lastName);
             this.email = new SimpleStringProperty(email);
-            
+
         }
 
         public String getFirstName() {
@@ -118,7 +164,7 @@ public class ListUserController implements Initializable {
         public String getEmail() {
             return email.get();
         }
-        
+
     }
-    
+
 }
