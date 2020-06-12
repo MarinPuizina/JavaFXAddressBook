9b8c155ba6f5705f840b83c5cf0f51f756c21662
MarinPuizina <marinpuizina@hotmail.com>
2020-06-12 15:51:20 +0200

    Add XML package, Persons XML Class and XMLHandler class

---
+++ src/XML/Persons.java
@@ -0,0 +1,18 @@
+package XML;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author marin
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Persons {
+
+    public Persons() {
+    }
+        
+}
---
+++ src/XML/XMLHandler.java
@@ -0,0 +1,79 @@
+package XML;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author marin
+ */
+public class XMLHandler {
+
+    private final String PERSONS_XML = "persons.xml";
+
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder;
+
+    //TODO 
+    //void findLastElement()
+    public void addElement(String firstName, String lastName, String email) {
+
+        try {
+
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(PERSONS_XML);
+
+            NodeList childNodes = doc.getChildNodes();
+            Element rootNode = (Element) childNodes.item(0);
+            System.out.println("root node --> " + rootNode.getNodeName());
+
+            Element personElement = doc.createElement("person");
+            // Adding email as an ID attribute so that application
+            // doesn't create additional Elements with the same Email
+            personElement.setAttribute("email", email);
+
+            // First Name
+            Element firstNameElement = doc.createElement("firstName");
+            firstNameElement.appendChild(doc.createTextNode(firstName));
+            personElement.appendChild(firstNameElement);
+
+            // Last Name
+            Element lastNameElement = doc.createElement("lastName");
+            lastNameElement.appendChild(doc.createTextNode(lastName));
+            personElement.appendChild(lastNameElement);
+
+            rootNode.appendChild(personElement);
+
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(PERSONS_XML);
+            transformer.transform(source, result);
+
+            System.out.println("Added element to the xml file.");
+
+        } catch (TransformerConfigurationException ex) {
+            Logger.getLogger(XMLHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException | SAXException | IOException | ParserConfigurationException ex) {
+            Logger.getLogger(XMLHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    //deleteElement()
+}
